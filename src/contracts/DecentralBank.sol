26c179d7890830b4d154385e62b5c2c00ceb1621
pavanleton <pavankumarkpkp@gmail.com>
2021-10-05 19:55:52 +0530

    add staking

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -11,9 +11,34 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaked;
+
+
     constructor(RWD _rwd,Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
+
+    }
+
+    function depositTokens(uint amount) public {
+
+        require(amount > 0, "Amount cannot be 0");
+
+        // Transfer Tether tokens
+        tether.transferFrom(msg.sender, address(this), amount);
+
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        if(!hasStaked){
+            stakers.push[msg.sender];
+        }
+
+        isStaked[msg.sender] = true;
+        hasStaked[msg.sender] = true;
 
     }
 
--- test/decentralbank.test.js
+++ test/decentralbank.test.js
@@ -1,4 +1,3 @@
-const {before} = require("truffle/build/484.bundled");
 const Tether = artifacts.require("Tether");
 const RWD = artifacts.require("RWD");
 const DecentralBank = artifacts.require("DecentralBank")
@@ -7,13 +6,22 @@ require("chai")
 .use(require("chai-as-promised"))
 .should();
 
-contract("DecentralBank", accounts =>{
+contract("DecentralBank", ([owner, customer]) =>{
     let tether, rwd, decentralBank;
+
+    function tokens(number) {
+        return web3.utils.toWei(number, "ether");
+    }
 
     before(async () => {
         tether = await Tether.new();
         rwd = await RWD.new();
-        decentralBank = await  DecentralBank.new();
+        decentralBank = await DecentralBank.new(rwd.address, tether.address);
+
+        await rwd.transfer(decentralBank.address, tokens("1000000"));
+
+        // Transfer 100 mock tethers to investor
+        await tether.transfer(customer, tokens("100"), {from: owner})
     })
 
     describe("Mock Tether Deployment" , async  () => {
@@ -24,6 +32,15 @@ contract("DecentralBank", accounts =>{
         it('should match name successfully ', async ()  => {
             const name =  await rwd.name();
             assert.equal(name, "Reward Token");
+
+        });
+        it('should match name successfully ', async ()  => {
+            const name =  await decentralBank.name();
+            assert.equal(name, "Decentral Bank");
+        });
+        it('contract has token ', async ()  => {
+            const balance = rwd.balanceOf(decentralBank.address);
+            assert.equal(balance, tokens("1000000"));
         });
     })
 } )
